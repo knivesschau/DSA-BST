9483dde3bea38844303000bddd14883d7cd4f7cf
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-08-02 22:56:42 -0700

    revisited drills

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,17 +1,18 @@
 class BinarySearchTree {
     constructor(key = null, value = null, parent = null) {
-        this.key = key;
+        this.key = key; 
         this.value = value;
-        this.parent = parent;
+        this.parent = parent; 
         this.left = null;
         this.right = null;
     }
-   
+
     insert(key, value) {
-        if (this.key == null) {
+        if (this.key === null) {
             this.key = key;
             this.value = value;
         }
+
         else if (key < this.key) {
             if (this.left == null) {
                 this.left = new BinarySearchTree(key, value, this);
@@ -20,8 +21,9 @@ class BinarySearchTree {
                 this.left.insert(key, value);
             }
         }
+
         else {
-            if (this.right == null) {
+            if (this.right === null) {
                 this.right = new BinarySearchTree(key, value, this);
             }
             else {
@@ -34,12 +36,46 @@ class BinarySearchTree {
         if (this.key == key) {
             return this.value;
         }
+
         else if (key < this.key && this.left) {
-            return this.value,left.find(key);
-        
+            return this.left.find(key);
         }
+
         else if (key > this.key && this.right) {
             return this.right.find(key);
+        }
+
+        else {
+            throw new Error('Key Error');
+        }
+    }
+
+    remove(key) {
+        if (this.key == key) {
+            if (this.left && this.right) {
+                const successor = this.right._findMin();
+                this.key = successor.key;
+                this.value = successor.value;
+                successor.remove(successor.key);
+            }
+
+            else if (this.left) {
+                this._replaceWith(this.left);
+            }
+
+            else if (this.right) {
+                this._replaceWith(this.right);
+            }
+
+            else {
+                this._replaceWith(null);
+            }
+        }
+        else if (key < this.key && this.left) {
+            this.left.remove(key);
+        }
+        else if (key > this.key && this.right) {
+            this.right.remove(key);
         }
         else {
             throw new Error('Key Error');
@@ -78,36 +114,7 @@ class BinarySearchTree {
         if (!this.left) {
             return this;
         }
-        return this.left._findMin()
-    }
-
-    remove(key) {
-        if (this.key == key) {
-            if (this.left && this.right) {
-                const successor = this.right._findMin();
-                this.key = successor.key;
-                this.value = successor.value;
-                successor.remove(successor.key);
-            }
-            else if (this.left) {
-                this._replaceWith(this.left);
-            }
-            else if (this.right) {
-                this._replaceWith(this.right);
-            }
-            else {
-                this._replaceWith(null);
-            }
-        }
-        else if (key < this.key && this.left) {
-            this.left.remove(key);
-        }
-        else if (key > this.key && this.right) {
-            this.right.remove(key);
-        }
-        else {
-            throw new Error('Key Error');
-        }
+        return this.left._findMin();
     }
 }
 
--- bst-drills.js
+++ bst-drills.js
@@ -5,42 +5,43 @@ const BinarySearchTree = require('./BinarySearchTree');
 function numberTree() {
     const BST = new BinarySearchTree();
 
-    BST.insert(3);
-    BST.insert(1);
-    BST.insert(4);
-    BST.insert(6);
-    BST.insert(9);
-    BST.insert(2);
-    BST.insert(5);
-    BST.insert(7);
+    BST.insert("3", "3");
+    BST.insert("1", "1");
+    BST.insert("4", "4");
+    BST.insert("6", "6");
+    BST.insert("9", "9");
+    BST.insert("2", "2");
+    BST.insert("5", "5");
+    BST.insert("7", "7");
 
-    console.log(BST); // binary tree skews to the right side, doesn't include values higher than 6? 
+    return BST;
 }
 
-numberTree();
+// console.log(numberTree());
 
 function letterTree() {
     const BST = new BinarySearchTree();
 
-    BST.insert('E');
-    BST.insert('A');
-    BST.insert('S');
-    BST.insert('Y');
-    BST.insert('Q');
-    BST.insert('U');
-    BST.insert('E');
-    BST.insert('S');
-    BST.insert('T');
-    BST.insert('I');
-    BST.insert('O');
-    BST.insert('N');
+    BST.insert('E', 'E');
+    BST.insert('A', 'A');
+    BST.insert('S', 'S');
+    BST.insert('Y', 'Y');
+    BST.insert('Q', 'Q');
+    BST.insert('U', 'U');
+    BST.insert('E', 'E');
+    BST.insert('S', 'S');
+    BST.insert('T', 'T');
+    BST.insert('I', 'I');
+    BST.insert('O', 'O');
+    BST.insert('N', 'N');
 
-    console.log(BST); // didn't print all the letters? 
+    return BST;
 }
 
-letterTree();
+// console.log(letterTree());
 
-// if there are no values in the tree, return 0. if there are values in the left and right branches, add them together for the sum. O(n) notation.
+// if there are no values in the tree, return 0. if there are values in the left and right branches, add them together for the sum. 
+// O(log(n)) or O(n) notation depending on if the tree is skewed or balanced.
 function tree(t){
     if(!t){
         return 0;
@@ -48,11 +49,37 @@ function tree(t){
     return tree(t.left) + t.value + tree(t.right)
 }
 
-function findHeight(BST) {
-    const distance = [];
-    _treeDistance(BST , 0, distance);
-    return Math .max(...distance);
+function findHeight(tree) {
+    if (!tree) {
+        return 0;
+    }
+
+    if (!tree.left && !tree.right) {
+        return 1;
+    }
+
+    let height = 0;
+
+    if (tree.right) {
+        let rightHeight = 1 + findHeight(tree.right);
+
+        if (rightHeight > height) {
+            height = rightHeight;
+        }
+    }
+    
+    if (tree.left) {
+        let leftHeight = 1 + findHeight(tree.left);
+        
+        if (leftHeight > height) {
+            height = leftHeight;
+        }
+    }
+    return height;
 }
+
+console.log(findHeight(letterTree()));
+console.log(findHeight(numberTree()));
 
 function _treeDistance(t, count = 0, arr = []) {
     if (!t) {
@@ -65,72 +92,121 @@ function _treeDistance(t, count = 0, arr = []) {
     }
 }
 
-function checkBST(BST) {
-    return _checkNodes(BST) === undefined;
+function checkBST(tree) {
+    if (!tree) {
+        return false;
+    }
+
+    if (tree.right) {
+        if (tree.right.key > tree.key) {
+            checkBST(tree.right);
+        }
+        else {
+            return false;
+        }
+    }
+
+    if (tree.left) {
+        if (tree.left.key < tree.key) {
+            checkBST(tree.left);
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
 }
 
-function _checkNodes(node) {
-    if (node.left) {
-        if (node.left.key > node.key) {
-            return false;
-        }
-        _checkNodes(node.left);
-    }
-    if (node.right) {
-        if (node.right.key < node.key) {
-            return false;
-        }
-        _checkNodes(node.right);
-    }
-}
+console.log(checkBST(numberTree()));
+console.log(checkBST(letterTree()));
 
 function getThirdLargest(tree) {
-    let valueSample = [tree.key, tree.key, tree.key];
-    _checkValues(tree, valueSample);
-    return tripleVal[0];
-}
+    const height = findHeight(tree);
 
-function _checkValues(node, arr) {
-    if (node.key > arr[0] && node.key < arr[i]) {
-        arr[0] = node.key;
+    if (height < 2) {
+        return null;
     }
-    else if (node.key > arr[1] && node.key < arr[2]) {
-        arr[0] = arr[1];
-        arr[1] = node.key;
+    else if (height < 3) {
+        if (tree.left && tree.right) {
+            return tree.left.value;
+        }
+        else {
+            return null;
+        }
     }
-    else if (node.key > arr[2]) {
-        arr[0] = arr[1];
-        arr[1] = arr[2];
-        arr[2] = node.key;
+    else if (height > 3) {
+        return getThirdLargest(tree.right);
     }
-    if (node.left) {
-        _checkValues(node.left, arr);
-    }
-    if (node.right) {
-        _checkValues(node.right, arr);
+    else {
+        return tree.key;
     }
 }
 
-function equidistBST(tree) {
-    const branchLengths = [];
-    _distMeasure(tree, 0, branchLengths);
-    const longestBranch = Math.max(...branchLengths);
-    const shortestBranch = Math.max(...branchLengths);
-    return (longestBranch - shortestBranch) <= 1;
+console.log(getThirdLargest(numberTree()));
+
+function balancedTree(tree) {
+    if (!tree) {
+        return false; 
+    }
+
+    if (!tree.right && !tree.left) {
+        return true;
+    }
+
+    if (Math.abs(findHeight(tree.right) - findHeight(tree.left)) > 1) {
+        return false;
+    }
+    return true;
 }
 
-function _distMeasure(t, count = 0, arr = []) {
-    if (t) {
-        count ++;
+const treeBalance = new BinarySearchTree();
+
+treeBalance.insert(5);
+treeBalance.insert(10);
+treeBalance.insert(2);
+
+console.log(balancedTree(numberTree()));
+console.log(balancedTree(letterTree()));
+console.log(balancedTree(treeBalance));
+
+function matchingTrees(array1, array2) {
+    if (array1.length !== array2.length || array1[0] !== array2[0]) {
+        return false;
     }
-    if (!t.left && !t.right) {
-        arr.push(count);
+
+    if (array1.length === 0 || array2.length === 0) {
+        return true;
     }
-    if (t.left) {
-        _distMeasure(t.left, count, arr);
+
+    const largerArray1 = [];
+    const largerArray2 = [];
+    const smallerArray1 = [];
+    const smallerArray2 = [];
+
+    for (let i = 1; i < array1.length; i++) {
+        if (array1[i] > array1[0]) {
+            largerArray1.push(array1[i]);
+        }
+        else {
+            smallerArray1.push(array1[i]);
+        }
     }
-    if (t.right) {
-        _distMeasure(t.right, count, arr);
+
+    for (let i = 1; i < array2.length; i++) {
+        if (array2[i] > array2[0]) {
+            largerArray2.push(array2[i]);
+        }
+        else {
+            smallerArray2.push(array2[i]);
+        }
     }
+
+    return (
+        matchingTrees(largerArray1, largerArray2) && matchingTrees(smallerArray1, smallerArray2)
+    );
 }
 
+const array1 = [3, 5, 4, 6, 1, 0, 2];
+const array2 = [3, 1, 5, 2, 4, 6, 0];
+
+console.log(matchingTrees(array1, array2));
